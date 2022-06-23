ecb5da3cf772e25fd23185a07c1a8e06b8d10e75
Kyle Awayan <kyle@awayan.com>
2022-06-23 15:41:09 -0700

    remove default variant

--- packages/core/src/Button/Button.tsx
+++ packages/core/src/Button/Button.tsx
@@ -1,9 +1,5 @@
 import MaterialButton, { ButtonProps } from "@mui/material/Button";
 
 export const Button = (props: ButtonProps) => {
-  return (
-    <MaterialButton variant="contained" {...props}>
-      {props.children}
-    </MaterialButton>
-  );
+  return <MaterialButton {...props}>{props.children}</MaterialButton>;
 };
