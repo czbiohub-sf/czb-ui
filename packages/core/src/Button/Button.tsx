243fd39f4aceac4ef7568aec84ee46a441123c1a
Kyle Awayan <kyle@awayan.com>
2022-06-23 15:36:18 -0700

    don't transform text actually

--- packages/core/src/Button/Button.tsx
+++ packages/core/src/Button/Button.tsx
@@ -2,11 +2,7 @@ import MaterialButton, { ButtonProps } from "@mui/material/Button";
 
 export const Button = (props: ButtonProps) => {
   return (
-    <MaterialButton
-      variant="contained"
-      sx={{ textTransform: "none", ...props.sx }}
-      {...props}
-    >
+    <MaterialButton variant="contained" {...props}>
       {props.children}
     </MaterialButton>
   );
