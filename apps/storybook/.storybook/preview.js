18484ed54ffe39bf66036254092f98ecb2729fef
Kyle Awayan <kyle@awayan.com>
2022-06-28 09:52:35 -0700

    import only specific weights on fonts

--- apps/storybook/.storybook/preview.js
+++ apps/storybook/.storybook/preview.js
@@ -1,6 +1,10 @@
 import { ThemeProvider } from "@czb-ui/core";
-import "@fontsource/barlow";
-import "@fontsource/lato";
+
+// Import fonts at their various weights
+// 400 is normal, 700 is bold
+import "@fontsource/barlow/700.css";
+import "@fontsource/lato/400.css";
+import "@fontsource/lato/700.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
