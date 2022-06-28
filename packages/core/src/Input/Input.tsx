25022375ff9fdd24cf4ee6867cc60f39eb9c801d
Kyle Awayan <kyle@awayan.com>
2022-06-28 11:45:11 -0700

    wip

--- packages/core/src/Input/Input.tsx
+++ packages/core/src/Input/Input.tsx
@@ -1,7 +1,14 @@
 import MaterialInputBase, { InputBaseProps } from "@mui/material/InputBase";
 import { styled } from "@mui/system";
 
-export const Input = styled(MaterialInputBase)<InputBaseProps>(({ theme }) => ({
+// https://mui.com/material-ui/customization/how-to-customize/#dynamic-overrides
+interface StyledInputBaseProps extends InputBaseProps {
+	error?: boolean
+}
+
+export const Input = styled(MaterialInputBase, {
+	shouldForwardProp: (prop) => prop !== 
+})<StyledInputBaseProps>(({ theme }) => ({
   border: "1px solid",
   borderColor: theme.palette.line.main,
   padding: "10px",
