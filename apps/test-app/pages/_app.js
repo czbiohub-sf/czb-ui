5b2401afa20eca2884f876ceeb689cd8081184a2
Kyle Awayan <kyle@awayan.com>
2022-07-01 10:21:25 -0700

    match storybook react version, import fonts

--- apps/test-app/pages/_app.js
+++ apps/test-app/pages/_app.js
@@ -1,5 +1,11 @@
 import { ThemeProvider } from "@czb-ui/core";
 
+// Import fonts at their various weights
+// 400 is normal, 700 is bold
+import "@fontsource/barlow/700.css";
+import "@fontsource/lato/400.css";
+import "@fontsource/lato/700.css";
+
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
