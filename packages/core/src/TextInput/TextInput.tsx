1b38c72a650fe99d8534bcf81a6f7c276310db60
Kyle Awayan <kyle@awayan.com>
2022-07-05 12:21:17 -0700

    get text input component ready for additions within the component

--- packages/core/src/TextInput/TextInput.tsx
+++ packages/core/src/TextInput/TextInput.tsx
@@ -1,10 +1,19 @@
 import MaterialInputBase, { InputBaseProps } from "@mui/material/InputBase";
 import { styled } from "@mui/material/styles";
 
-export const TextInput = styled(MaterialInputBase, {
+const TextBox = styled(MaterialInputBase, {
   shouldForwardProp: (prop) => prop !== "error",
 })<InputBaseProps>(({ error, theme }) => ({
   border: "1px solid",
   borderColor: error ? theme.palette.error.main : theme.palette.line.main,
   padding: "5px",
 })) as typeof MaterialInputBase;
+
+// Would extending InputBaseProps be right here?
+// I would think extending TextBox's props would
+// be correct but I'm not sure how to extend the const
+interface TextInputProps extends InputBaseProps {}
+
+export const TextInput = ({ ...props }) => {
+  return <TextBox {...props} />;
+};
