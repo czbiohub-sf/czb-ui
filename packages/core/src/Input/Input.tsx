1191abb7c9a72cf6dfa69b0f1a6f5d3b3626d5d7
Kyle Awayan <kyle@awayan.com>
2022-07-05 11:39:00 -0700

    make outline

--- packages/core/src/Input/Input.tsx
+++ packages/core/src/Input/Input.tsx
@@ -1,6 +1,8 @@
 import MaterialInputBase, { InputBaseProps } from "@mui/material/InputBase";
 import { styled } from "@mui/material/styles";
 
-export const Input = styled(MaterialInputBase)<InputBaseProps>(
-  () => ({})
-) as typeof MaterialInputBase;
+export const Input = styled(MaterialInputBase)<InputBaseProps>(({ theme }) => ({
+  border: "1px solid",
+  borderColor: theme.palette.line.main,
+  padding: "5px",
+})) as typeof MaterialInputBase;
