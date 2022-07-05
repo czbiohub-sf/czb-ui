46836340c853d4e6fef4016e06838612eda3d308
Kyle Awayan <kyle@awayan.com>
2022-07-05 14:41:21 -0700

    use unique id for accessiblity

--- packages/core/src/TextInput/TextInput.tsx
+++ packages/core/src/TextInput/TextInput.tsx
@@ -1,6 +1,7 @@
 import { Box, Typography, InputLabel } from "@mui/material";
 import MaterialInputBase, { InputBaseProps } from "@mui/material/InputBase";
 import { styled } from "@mui/material/styles";
+import { v4 as uuidv4 } from "uuid";
 
 // We use shouldForwardProp to not pass the additonal props
 // added for this component (at the interface extend) to the
@@ -27,14 +28,23 @@ interface TextInputProps extends InputBaseProps {
   label?: string;
 }
 
+// Create an unique ID so the input is linked to
+// the label and helper text
+// https://mui.com/material-ui/react-text-field/#accessibility
+const uniqueId = uuidv4();
+
 export const TextInput = (props: TextInputProps) => {
   const { error, errorDesc, label } = props;
 
   return (
     <Box>
-      <InputLabel>{label}</InputLabel>
-      <TextBox {...props} />
-      {error && <Typography color="error">{errorDesc}</Typography>}
+      <InputLabel htmlFor={uniqueId}>{label}</InputLabel>
+      <TextBox id={uniqueId} {...props} />
+      {error && (
+        <Typography id={uniqueId} color="error">
+          {errorDesc}
+        </Typography>
+      )}
     </Box>
   );
 };
