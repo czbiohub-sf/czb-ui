4d97f379a7b5aa0e8ee44fd9787228b13a416799
Kyle Awayan <kyle@awayan.com>
2022-07-08 09:35:29 -0700

    successfully add czifui to czb-ui!!

--- packages/core/src/Button/Button.tsx
+++ packages/core/src/Button/Button.tsx
@@ -1,5 +1,9 @@
 import { Button as CZIFUIButton } from "czifui";
 
 export const Button = () => {
-  return <CZIFUIButton>I am CZI Button</CZIFUIButton>;
+  return (
+    <CZIFUIButton sdsStyle="rounded" sdsType="primary">
+      I am CZI Button
+    </CZIFUIButton>
+  );
 };
---
+++ packages/core/src/theme.ts
@@ -0,0 +1,46 @@
+import { createTheme } from "@mui/material/styles";
+import { defaultAppTheme, makeThemeOptions } from "czifui";
+
+const primaryColors = {
+  "100": "#FCFAFF",
+  "200": "#F6F2FC",
+  "300": "#D4BFFF",
+  "400": "#511CC1",
+  "500": "#4317A1",
+  "600": "#371680",
+};
+
+const infoColors = {
+  "100": "#F6F2FC",
+  "200": "#F3EDFC",
+  "400": "#511CC1",
+  "600": "#371680",
+};
+
+const primaryBorders = {
+  300: `1px solid ${primaryColors[300]}`,
+  400: `1px solid ${primaryColors[400]}`,
+  500: `1px solid ${primaryColors[500]}`,
+  600: `1px solid ${primaryColors[600]}`,
+  dashed: `2px dashed ${primaryColors[400]}`,
+};
+
+const fontFamily = ["Lato", "Barlow"].join(",");
+
+const appTheme = { ...defaultAppTheme };
+
+appTheme.colors.primary = primaryColors;
+appTheme.colors.info = infoColors;
+appTheme.typography.fontFamily = fontFamily;
+
+appTheme.borders = appTheme.borders ?? {
+  error: {},
+  gray: {},
+  link: {},
+  primary: {},
+  success: {},
+  warning: {},
+};
+appTheme.borders.primary = primaryBorders;
+
+export const biohubTheme = createTheme(makeThemeOptions(appTheme));
