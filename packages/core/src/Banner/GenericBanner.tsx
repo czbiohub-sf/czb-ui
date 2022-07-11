2ad337c508670f6749c0c6d5d9a59642d406ca0b
Kyle Awayan <kyle@awayan.com>
2022-07-11 16:40:21 -0700

    change banner styles

--- packages/core/src/Banner/GenericBanner.tsx
+++ packages/core/src/Banner/GenericBanner.tsx
@@ -22,10 +22,20 @@ export const GenericBanner = ({
     >
       <Center justifyContent="left">
         <Container maxWidth="lg" sx={{ color: "white", zIndex: 1 }}>
-          <Typography variant="h1" gutterBottom={false}>
+          <Typography
+            fontSize="2.5rem"
+            variant="h1"
+            component="div"
+            gutterBottom={false}
+          >
             {title}
           </Typography>
-          <Typography variant="h2" gutterBottom={false} marginTop="0.5rem">
+          <Typography
+            variant="h2"
+            component="div"
+            gutterBottom={false}
+            marginTop="0.5rem"
+          >
             {subtitle}
           </Typography>
         </Container>
