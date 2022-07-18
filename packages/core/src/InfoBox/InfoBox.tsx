f499351009dbe57a0b57c0866179d4072280fe2a
Kyle Awayan <kyle@awayan.com>
2022-07-18 16:25:39 -0700

    style info box

--- packages/core/src/InfoBox/InfoBox.tsx
+++ packages/core/src/InfoBox/InfoBox.tsx
@@ -31,7 +31,14 @@ export const InfoBox = ({ image }: ImageButtonProps) => {
         }}
       >
         <Box flex={3}>{image}</Box>
-        <Box flex={2} sx={{ display: "flex", flexDirection: "column" }}>
+        <Box
+          flex={2}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
           <Typography variant="h2" component="div">
             The Tabula Sapiens: A multiple-organ, single-cell transcriptomic
             atlas of humans
@@ -39,7 +46,7 @@ export const InfoBox = ({ image }: ImageButtonProps) => {
           <Typography>
             The Tabula Sapiens Consortium, Science 376, eabl4896 (2022).
           </Typography>
-          <Link sx={{ marginTop: "auto" }}>Explore Tools</Link>
+          <Link sx={{ marginTop: "1rem" }}>Explore Tools</Link>
         </Box>
       </Box>
     </Paper>
