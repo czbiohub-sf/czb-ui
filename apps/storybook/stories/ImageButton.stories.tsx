cce4fc2d1c0a8f65b0fe9d1b04c2483a27ef18ca
Kyle Awayan <kyle@awayan.com>
2022-07-18 15:45:02 -0700

    make draft image button

--- apps/storybook/stories/ImageButton.stories.tsx
+++ apps/storybook/stories/ImageButton.stories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { ImageButton as CZBUIImageButton } from "@czb-ui/core/src";
+import sampleImage from "./assets/sapiens_banner.webp";
 
 export default {
   title: "Image Button",
@@ -8,7 +9,18 @@ export default {
   argTypes: {},
 };
 
-const Template = (args: any) => <CZBUIImageButton />;
+const Template = (args: any) => (
+  <CZBUIImageButton
+    image={
+      <img
+        style={{ objectFit: "cover", height: "100%", width: "100%", zIndex: 0 }}
+        src={args.image}
+      />
+    }
+  />
+);
 
 export const ImageButton = Template.bind({});
-ImageButton.args = {};
+ImageButton.args = {
+  image: sampleImage,
+};
--- packages/core/src/ImageButton/ImageButton.tsx
+++ packages/core/src/ImageButton/ImageButton.tsx
@@ -1,3 +1,48 @@
-export const ImageButton = () => {
-  return <div>test</div>;
+import { Box, Paper, Typography } from "@mui/material";
+
+interface ImageButtonProps {
+  image?: React.ReactNode;
+}
+
+export const ImageButton = ({ image }: ImageButtonProps) => {
+  // TODO: Put grey colors in palette
+  return (
+    <Paper
+      sx={{
+        height: "700px",
+        width: "400px",
+        position: "relative",
+        border: "1px solid",
+        borderColor: "#f1f0f0",
+        padding: "1rem",
+      }}
+      elevation={0}
+      square
+    >
+      <Box
+        sx={{
+          zIndex: 1,
+          bgcolor: "#f9f9f9",
+          height: "100%",
+          padding: "20px",
+        }}
+      >
+        <Box
+          sx={{
+            top: 0,
+            width: "100%",
+            height: "350px",
+          }}
+        >
+          {image}
+        </Box>
+        <Typography variant="h2" component="div">
+          Tools
+        </Typography>
+        <Typography>
+          Utilise informatics tools developed by the CZ Biohub.
+        </Typography>
+      </Box>
+    </Paper>
+  );
 };
