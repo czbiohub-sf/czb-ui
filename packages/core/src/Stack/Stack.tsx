3fe7a71cbdd78b77909c84c789dbc10680b07a99
Kyle Awayan <kyle@awayan.com>
2022-07-19 11:47:24 -0700

    remove box and stack components that were just re-exported from mui

--- packages/core/src/Stack/Stack.tsx
+++
@@ -1,6 +0,0 @@
-import MaterialStack, { StackProps } from "@mui/material/Stack";
-import { styled } from "@mui/material/styles";
-
-export const Stack = styled(MaterialStack)<StackProps>(
-  () => ({})
-) as typeof MaterialStack;
