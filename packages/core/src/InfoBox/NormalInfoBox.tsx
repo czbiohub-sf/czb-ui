e36d55382abb5ff1669b60bd7bdff38b594ddf01
Kyle Awayan <kyle@awayan.com>
2022-07-22 10:05:59 -0700

    start making normal info box reactive

--- packages/core/src/InfoBox/NormalInfoBox.tsx
+++ packages/core/src/InfoBox/NormalInfoBox.tsx
@@ -1,20 +1,34 @@
-import { Box, Typography } from "@mui/material";
+import { Typography, styled } from "@mui/material";
+import Box, { BoxProps } from "@mui/material/Box";
 import { Link } from "czifui";
 import { InfoBoxProps } from "./InfoBox";
 
+interface NormalInfoBoxContainerProps extends BoxProps {
+  imageOnRight?: boolean;
+}
+
+const NormalInfoBoxContainer = styled(Box, {
+  shouldForwardProp: (prop) => prop != "imageOnRight",
+})<NormalInfoBoxContainerProps>(({ imageOnRight, theme }) => ({
+  zIndex: 1,
+  background: "#f9f9f9",
+  height: "100%",
+  display: "flex",
+  gap: "50px",
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "column",
+  },
+  [theme.breakpoints.up("sm")]: {
+    flexDirection: imageOnRight ? "row-reverse" : "row",
+  },
+}));
+
 export default function NormalInfoBox({ image, imageOnRight }: InfoBoxProps) {
   return (
-    <Box
-      sx={{
-        zIndex: 1,
-        bgcolor: "#f9f9f9",
-        height: "100%",
-        display: "flex",
-        flexDirection: imageOnRight ? "row-reverse" : "row",
-        gap: "50px",
-      }}
-    >
-      <Box flex={3}>{image}</Box>
+    <NormalInfoBoxContainer imageOnRight={imageOnRight}>
+      <Box flex={3} height="100%">
+        {image}
+      </Box>
       <Box
         flex={2}
         sx={{
@@ -32,6 +46,6 @@ export default function NormalInfoBox({ image, imageOnRight }: InfoBoxProps) {
         </Typography>
         <Link sx={{ marginTop: "1rem" }}>Explore Tools</Link>
       </Box>
-    </Box>
+    </NormalInfoBoxContainer>
   );
 }
