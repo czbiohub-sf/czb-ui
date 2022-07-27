08e8fbf971598dc291ec9ee31816d8e73c0d4e5b
Kyle Awayan <kyle@awayan.com>
2022-07-27 14:16:39 -0700

    add block switcher to stack

--- packages/tina-cms/src/components/Stack/StackBlock.tsx
+++ packages/tina-cms/src/components/Stack/StackBlock.tsx
@@ -12,11 +12,24 @@ export const StackBlock = (props: any) => {
         spacing={{ xs: 2, md: 3 }}
         columns={{ xs: 4, sm: 8, md: 12 }}
       >
-        {blocks.map((block, i) => (
-          <Grid item xs={2} sm={4} md={4} key={i}>
-            {"test"}
-          </Grid>
-        ))}
+        {blocks.map((nestedBlock, i) => {
+          // Since nested lists get their typename changed,
+          // pass the right one for the block switcher component
+          const blockTypeNameSplit = nestedBlock.__typename.split(/(?=[A-Z])/);
+          const newBlockTypeName = blockTypeNameSplit
+            .slice(0, 2)
+            .concat(blockTypeNameSplit.slice(-2))
+            .join("");
+          nestedBlock.__typename = newBlockTypeName;
+
+          console.log(nestedBlock);
+
+          return (
+            <Grid item xs={2} sm={4} md={4} key={i}>
+              <BlockSwitcher blocks={[nestedBlock]} />
+            </Grid>
+          );
+        })}
       </Grid>
     </Container>
   );
