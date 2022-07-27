3556fcadc740ca035bcc3dfcbe0a2586f2ab1dbb
Kyle Awayan <kyle@awayan.com>
2022-07-27 00:23:37 -0700

    add drawer to mobile nav bar

--- packages/core/src/NavBar/MobilePagesMenu.tsx
+++ packages/core/src/NavBar/MobilePagesMenu.tsx
@@ -1,6 +1,8 @@
 import { PagesObject } from "./NavBar";
-import { Box, Button } from "@mui/material";
+import { Box, Drawer, ListItemButton } from "@mui/material";
 import Hamburger from "hamburger-react";
+import { useState } from "react";
+import { Link } from "czifui";
 
 interface MobilePagesMenuProps {
   pages: Array<PagesObject>;
@@ -11,9 +13,33 @@ export const MobilePagesMenu = ({
   pages,
   pagesComponent,
 }: MobilePagesMenuProps) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <Box>
-      <Hamburger />
+      <Hamburger toggled={open} toggle={setOpen} />
+      <Drawer
+        variant="persistent"
+        anchor="top"
+        open={open}
+        onClose={() => setOpen(false)}
+        sx={{
+          "& .MuiDrawer-paper": {
+            top: "50px",
+          },
+        }}
+      >
+        {pages.map((page) => (
+          <Link
+            color="inherit"
+            component={pagesComponent}
+            to={page.to}
+            sx={{ mx: 5 }}
+          >
+            <ListItemButton>{page.title}</ListItemButton>
+          </Link>
+        ))}
+      </Drawer>
     </Box>
   );
 };
