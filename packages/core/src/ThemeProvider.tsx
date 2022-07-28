86c9660afdeefc3e13d30322539945a9e304cada
Kyle Awayan <kyle@awayan.com>
2022-07-28 13:04:41 -0700

    add dark mode prop to theme provider

--- packages/core/src/ThemeProvider.tsx
+++ packages/core/src/ThemeProvider.tsx
@@ -1,16 +1,27 @@
 import * as React from "react";
-import { ThemeProvider as MUIThemeProvider } from "@mui/material/styles";
+import {
+  ThemeProvider as MUIThemeProvider,
+  createTheme,
+} from "@mui/material/styles";
 import { biohubTheme } from "./theme";
 
 import CssBaseline from "@mui/material/CssBaseline";
 
 interface ThemeProviderProps {
   children: React.ReactNode;
+  darkMode?: boolean;
 }
 
-export const ThemeProvider = ({ children }: ThemeProviderProps) => {
+export const ThemeProvider = ({ children, darkMode }: ThemeProviderProps) => {
+  const finalTheme = createTheme({
+    ...biohubTheme,
+    palette: {
+      mode: darkMode ? "dark" : "light",
+    },
+  });
+
   return (
-    <MUIThemeProvider theme={biohubTheme}>
+    <MUIThemeProvider theme={finalTheme}>
       <CssBaseline />
       {children}
     </MUIThemeProvider>
