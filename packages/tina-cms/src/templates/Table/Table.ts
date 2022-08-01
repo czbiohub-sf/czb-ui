737ea57ddb290390a3b4e116365c7671787a7341
Kyle Awayan <kyle@awayan.com>
2022-08-01 09:43:10 -0700

    change csv limit and add a validation

--- packages/tina-cms/src/templates/Table/Table.ts
+++ packages/tina-cms/src/templates/Table/Table.ts
@@ -7,7 +7,7 @@ export const Table: TinaTemplate = {
   fields: [
     {
       type: "string",
-      label: "Paste CSV here (small CSVs only, less than 100KB)",
+      label: "Paste CSV here (small CSVs only, less than ~30KB)",
       description:
         "Please do not use this as a primary way of editing the CSV.",
       name: "csvData",
@@ -15,6 +15,17 @@ export const Table: TinaTemplate = {
         component: "textarea",
         parse: (val?: string) => val && JSON.stringify(csvParse(val)),
         format: (val?: string) => val && csvFormat(JSON.parse(val)),
+        validate: (val?: string) => {
+          // val is the stringified JSON of csvParse()
+          if (!val) {
+            return;
+          }
+
+          // TODO: Test this limit
+          if (val.length > 50000) {
+            return "This CSV is too big. A custom Table implementation is needed.";
+          }
+        },
       },
     },
   ],
