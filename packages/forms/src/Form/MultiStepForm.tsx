f17ea3ad20fd291ac272f34f48c7f15537cf26be
Kyle Awayan <kyle@awayan.com>
2022-11-04 18:42:34 -0700

    change saving form data behavior in between steps

--- packages/forms/src/Form/MultiStepForm.tsx
+++ packages/forms/src/Form/MultiStepForm.tsx
@@ -26,8 +26,10 @@ export const MultiStepForm = ({
 
   const currentStep = schema.length - remSteps;
 
-  // TODO: Find type of submit event?
-  const onStepSubmit = (e: any) => {
+  // Since we want to keep the form data at each step,
+  // it's better to just handle the form's state manually
+  // so we can retrieve the form data anytime without onSubmit
+  const onFormChange = (e: any) => {
     // TODO: make sure there are no edge cases
     // with this setting formData thing...
     // (e.g. going back a step then going foward,
@@ -36,7 +38,10 @@ export const MultiStepForm = ({
       copiedFormData[currentStep] = e.formData;
       return copiedFormData;
     });
+  };
 
+  // TODO: Find type of submit event?
+  const onStepSubmit = (e: any) => {
     // Since the state is changed after,
     // (so we don't go out of bounds on the
     // RJSF form component, *then* check if the
@@ -58,6 +63,7 @@ export const MultiStepForm = ({
       onSubmit={onStepSubmit}
       formData={formData[currentStep]}
       widgets={widgets}
+      onChange={onFormChange}
     >
       <FormPageNav
         steps={steps}
