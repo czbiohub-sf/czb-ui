bf932916de38ef8dceaf3c60762bd215e42857f0
Kyle Awayan <kyle@awayan.com>
2022-11-19 20:05:25 -0800

    add another test

--- packages/forms/src/__tests__/Form.tsx
+++ packages/forms/src/__tests__/Form.tsx
@@ -1,4 +1,6 @@
-import { render } from "@testing-library/react";
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Form from "../Form/Form";
 
 const sampleSchema = {
@@ -16,6 +18,25 @@ const sampleSchema = {
 
 it("renders", () => {
   render(
-    <Form schema={{ sampleSchema }} onCompleteSubmit={(e) => console.log(e)} />
+    <Form schema={sampleSchema} onCompleteSubmit={(e) => console.log(e)} />
+  );
+});
+
+it("submits the correct data", async () => {
+  const user = userEvent.setup();
+  const handleSubmit = jest.fn();
+
+  render(<Form schema={sampleSchema} onCompleteSubmit={handleSubmit} />);
+
+  await user.type(screen.getByLabelText(/name/i), "abcdef");
+  await user.type(screen.getByLabelText(/age/i), "324829034");
+
+  await user.click(screen.getByRole("button", { name: /submit/i }));
+
+  await waitFor(() =>
+    expect(handleSubmit).toHaveBeenCalledWith({
+      name: "abcdef",
+      age: 324829034,
+    })
   );
 });
