3b8db590d6e55d36550acf9c0da7d7f6f229b514
Kyle Awayan <kyle@awayan.com>
2023-01-07 12:09:51 -0800

    start creating docs menu

--- apps/docs/components/DocsMenu/DocsMenu.js
+++ apps/docs/components/DocsMenu/DocsMenu.js
@@ -1,5 +1,38 @@
 import React from "react";
+import { Link } from "czifui";
+import {
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+
+const drawerWidth = 240;
 
 export default function DocsMenu() {
-  return <div>DocsMenu</div>;
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{
+        width: drawerWidth,
+        [`& .MuiDrawer-paper`]: {
+          width: drawerWidth,
+          boxSizing: "border-box",
+          position: "relative",
+        },
+      }}
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  );
 }
--- apps/docs/components/DocsPageLayout/DocsPageLayout.js
+++ apps/docs/components/DocsPageLayout/DocsPageLayout.js
@@ -3,7 +3,7 @@ import { Stack, Box } from "@mui/material";
 
 export default function DocsPageLayout({ sidebar, children }) {
   return (
-    <Stack>
+    <Stack direction="row">
       <Box>{sidebar}</Box>
       <Box>{children}</Box>
     </Stack>
