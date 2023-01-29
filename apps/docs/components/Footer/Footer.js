b39dbcdd33b661bf14b34ecd2b811ec3664dca80
Kyle Awayan <kyle@awayan.com>
2023-01-29 09:30:33 -0800

    add footer items

--- apps/docs/components/Footer/Footer.js
+++ apps/docs/components/Footer/Footer.js
@@ -1,11 +1,41 @@
 import { Footer as CZBUIFooter } from "@czb-ui/core";
 import { BiohubLogo } from "@czb-ui/biohub-logos";
 
+const pages = [
+  {
+    title: "czb-ui",
+    pages: [
+      {
+        title: "Documentation",
+        to: "/docs",
+      },
+      {
+        title: "GitHub",
+        to: "https://github.com/czbiohub/czb-ui",
+        externalLink: true,
+        newTab: true, // newTab doesn't work as of now
+      },
+    ],
+  },
+  {
+    title: "Company",
+    pages: [
+      {
+        title: "CZ Biohub",
+        to: "https://www.czbiohub.org/",
+        externalLink: true,
+        newTab: true, // newTab doesn't work as of now
+      },
+    ],
+  },
+];
+
 export default function Footer() {
   return (
     <CZBUIFooter
       title="CZ BIOHUB"
       logo={<BiohubLogo sx={{ fonSize: "2rem" }} />}
+      pages={pages}
     />
   );
 }
