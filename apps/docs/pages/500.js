fbb8f995207c0fab67a6cd9545a7a582aa44f6f2
Kyle Awayan <kyle@awayan.com>
2023-02-06 18:56:01 -0800

    add custom 404 and 500 pages

---
+++ apps/docs/pages/500.js
@@ -0,0 +1,16 @@
+import { Box, Typography } from "@mui/material";
+
+export default function Custom404() {
+  return (
+    <Box
+      display="flex"
+      height="100%"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection="column"
+    >
+      <Typography variant="h1">500</Typography>
+      <Typography>Server-side error occurred</Typography>
+    </Box>
+  );
+}
