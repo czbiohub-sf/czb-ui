34e0f6844086fbda5e45b87a8d61a9f83ede6608
Kyle Awayan <kyle@awayan.com>
2023-03-26 20:35:20 -0700

    add custom styles for headers

--- packages/core/src/theme.ts
+++ packages/core/src/theme.ts
@@ -20,4 +20,31 @@ appTheme.typography.fontFamily = mainFont;
 const fontBodyM = defaultAppTheme.typography.styles.body.m;
 appTheme.typography.styles.body["xs"] = fontBodyM;
 
+// Change h1 font
+// (SDS uses "header.xxl" for MUI's "h1")
+// https://github.com/chanzuckerberg/sci-components/blob/d607e6d8c38bf8da367844f706b314b33ab83012/src/core/styles/common/defaultTheme.ts#L404
+const newFontHeaderXxl = {
+  ...defaultAppTheme.typography.styles.header.xxl,
+  fontFamily: "STIX Two Text",
+};
+appTheme.typography.styles.header.xxl = newFontHeaderXxl;
+
+// Change h2 font
+// (SDS uses "header.xl" for MUI's "h2")
+// (https://github.com/chanzuckerberg/sci-components/blob/d607e6d8c38bf8da367844f706b314b33ab83012/src/core/styles/common/defaultTheme.ts#L405)
+const newFontHeaderXl = {
+  ...defaultAppTheme.typography.styles.header.xl,
+  fontFamily: "STIX Two Text",
+};
+appTheme.typography.styles.header.xl = newFontHeaderXl;
+
+// Change h3 font
+// (SDS uses "header.l" for MUI's "h3")
+// (https://github.com/chanzuckerberg/sci-components/blob/d607e6d8c38bf8da367844f706b314b33ab83012/src/core/styles/common/defaultTheme.ts#L406)
+const newFontHeaderL = {
+  ...defaultAppTheme.typography.styles.header.l,
+  fontFamily: "Lato",
+};
+appTheme.typography.styles.header.l = newFontHeaderL;
+
 export const biohubTheme = createTheme(makeThemeOptions(appTheme));
