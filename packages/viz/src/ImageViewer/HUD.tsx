d425c2db8f38b4fd40c8dfb6300f693e4ffbccbf
Kyle Awayan <kyle@awayan.com>
2023-05-15 14:05:41 -0700

    use white background

--- packages/viz/src/ImageViewer/HUD.tsx
+++ packages/viz/src/ImageViewer/HUD.tsx
@@ -10,9 +10,8 @@ export default function HUD({ children }: HUDProps) {
     <Box
       sx={{
         position: "absolute",
-        backgroundColor: "rgba(0, 0, 0, 0.8)",
+        backgroundColor: "rgba(255, 255, 255, 0.8)",
         padding: 4,
-        color: "white",
         zIndex: 2, // Overlay on top of deck.gl
       }}
     >
