32bd0338d360887f39b24de0cbda93d86196d632
Kyle Awayan <kyle@awayan.com>
2023-05-15 15:11:29 -0700

    style HUD

--- packages/viz/src/ImageViewer/HUD.tsx
+++ packages/viz/src/ImageViewer/HUD.tsx
@@ -10,8 +10,10 @@ export default function HUD({ children }: HUDProps) {
     <Box
       sx={{
         position: "absolute",
-        backgroundColor: "rgba(255, 255, 255, 0.8)",
-        padding: 4,
+        backgroundColor: "rgba(255, 255, 255, 0.9)",
+        backdropFilter: "blur(10px)",
+        margin: 6,
+        padding: 7,
         zIndex: 2, // Overlay on top of deck.gl
       }}
     >
