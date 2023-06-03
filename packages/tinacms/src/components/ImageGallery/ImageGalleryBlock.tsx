c684e8f6c4907aa87b66e87b0e0af0b57921f8b3
Kyle Awayan <kyle@awayan.com>
2023-06-04 01:00:54 +0900

    add fallback when there is no image yet, improve styles

--- packages/tinacms/src/components/ImageGallery/ImageGalleryBlock.tsx
+++ packages/tinacms/src/components/ImageGallery/ImageGalleryBlock.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Grid } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
 import Image from "next/image";
 
 interface Image {
@@ -18,25 +18,50 @@ interface ImageGalleryProps {
 }
 
 export const ImageGalleryBlock = ({ block }: ImageGalleryProps) => {
+  const images = block.images;
+
   return (
     <Container>
       <Grid
         container
         spacing={7}
-        columns={{ xs: 2, sm: 8, md: block.images?.length > 2 ? 12 : 8 }}
+        columns={{ xs: 2, sm: 8, md: images?.length > 2 ? 12 : 8 }}
         sx={{ my: 6 }}
       >
-        {block.images.map((image, i) => (
-          <Box sx={{ position: "relative", height: "300px", width: "50%" }}>
-            <Image
-              src={image.image}
-              alt={image.imageAlt}
-              layout="fill"
-              objectFit="contain"
-              sizes="(max-width: 768px) 100vw, 50w"
-            />
-          </Box>
-        ))}
+        {images &&
+          images.map((imageObj, i) => (
+            <Grid item sx={{ height: "300px", width: "50%" }}>
+              {imageObj.image && (
+                <Box
+                  sx={{ position: "relative", height: "100%", width: "100%" }}
+                >
+                  <Image
+                    src={imageObj.image}
+                    alt={imageObj.imageAlt}
+                    layout="fill"
+                    objectFit="contain"
+                    sizes="(max-width: 768px) 100vw, 50w"
+                  />
+                </Box>
+              )}
+              {!imageObj.image && (
+                <Box
+                  sx={{
+                    background: "grey",
+                    height: "100%",
+                    color: "white",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Typography variant="h1" component="div">
+                    Please put an image
+                  </Typography>
+                </Box>
+              )}
+            </Grid>
+          ))}
       </Grid>
     </Container>
   );
