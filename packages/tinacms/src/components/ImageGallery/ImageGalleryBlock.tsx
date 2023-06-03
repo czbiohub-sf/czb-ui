acc81f4082941748899ef5f0b4cfddff7a7ed3f2
Kyle Awayan <kyle@awayan.com>
2023-06-03 18:51:02 +0900

    code image gallery

--- packages/tinacms/src/components/ImageGallery/ImageGalleryBlock.tsx
+++ packages/tinacms/src/components/ImageGallery/ImageGalleryBlock.tsx
@@ -1,3 +1,6 @@
+import { Box, Container, Grid } from "@mui/material";
+import Image from "next/image";
+
 interface Image {
   __typename: "PageBlocksImageGalleryImages";
   image: string;
@@ -15,5 +18,26 @@ interface ImageGalleryProps {
 }
 
 export const ImageGalleryBlock = ({ block }: ImageGalleryProps) => {
-  return <div>ImageGalleryBlock</div>;
+  return (
+    <Container>
+      <Grid
+        container
+        spacing={7}
+        columns={{ xs: 2, sm: 8, md: block.images?.length > 2 ? 12 : 8 }}
+        sx={{ my: 6 }}
+      >
+        {block.images.map((image, i) => (
+          <Box sx={{ position: "relative", height: "300px", width: "50%" }}>
+            <Image
+              src={image.image}
+              alt={image.imageAlt}
+              layout="fill"
+              objectFit="contain"
+              sizes="(max-width: 768px) 100vw, 50w"
+            />
+          </Box>
+        ))}
+      </Grid>
+    </Container>
+  );
 };
