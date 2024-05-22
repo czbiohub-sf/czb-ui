67421ddade298df54df70f64b574e6eec3a4e693
Kyle Awayan <kyle@awayan.com>
2024-05-22 14:16:24 -0700

    sequential colors

--- packages/viz/src/3DUmap/colors.ts
+++ packages/viz/src/3DUmap/colors.ts
@@ -1,13 +1,15 @@
-import { scaleOrdinal, rgb, schemeCategory10 } from "d3";
+import { scaleSequential, rgb, interpolateCool } from "d3";
 
 function convertIntTypedArrayToCategoryColors(
   intTypedArray: Int32Array
 ): Uint8Array {
   // intTypedArray looks like [0, 0, 2, 1, 2] where each number is a category
-  const colorScale = scaleOrdinal(schemeCategory10);
+  const colorScale = scaleSequential(interpolateCool);
+  const maxValue = Math.max(...intTypedArray);
+
   const categoryColors = new Uint8Array(intTypedArray.length * 3);
   for (let i = 0; i < intTypedArray.length; i++) {
-    const color = rgb(colorScale(intTypedArray[i].toString()));
+    const color = rgb(colorScale(intTypedArray[i] / maxValue));
     categoryColors[i * 3] = color.r;
     categoryColors[i * 3 + 1] = color.g;
     categoryColors[i * 3 + 2] = color.b;
