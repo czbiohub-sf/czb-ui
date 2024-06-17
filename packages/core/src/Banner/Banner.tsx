98fb40791a1b111c804e64a88fac3f85c534b0b5
Kyle Awayan <kyle@awayan.com>
2024-06-17 15:35:45 -0700

    banner refactor

--- packages/core/src/Banner/Banner.tsx
+++ packages/core/src/Banner/Banner.tsx
@@ -1,88 +1,108 @@
-import { Box, Container, Typography } from "@mui/material";
 import React from "react";
-import { PageLink } from "../UniversalTypes/links";
+import { Box, Container, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import {
+  CommonThemeProps,
+  getColors,
+  fontHeaderXxl,
+} from "@czi-sds/components";
+import { css } from "@emotion/react";
 
-interface BannerProps {
+interface BannerProps extends CommonThemeProps {
   headline?: string;
-  page?: PageLink;
-  pagesComponent?: any; // TODO: Find type of mui link component prop
   image?: React.ReactNode;
   type?: "background" | "nobackground";
 }
 
 const maxHeight = "500px";
 
+const StyledBox = styled(Box)<BannerProps>((props) => {
+  const colors = getColors(props);
+
+  return css`
+    position: relative;
+    height: ${maxHeight};
+    width: 100%;
+    background-color: ${colors?.common.black};
+  `;
+});
+
+const StyledContainer = styled(Container)<BannerProps>(() => ({
+  position: "relative",
+  zIndex: 2,
+  height: "100%",
+}));
+
+const ContentBox = styled(Box)<BannerProps>((props) => {
+  return css`
+    display: flex;
+    justify-content: left;
+    height: 100%;
+    padding-top: ${props.theme.spacing(7)};
+    padding-left: ${props.theme.spacing(7)};
+    align-items: center;
+    max-width: 500px;
+
+    ${props.theme.breakpoints.down("md")} {
+      align-items: top;
+    }
+  `;
+});
+
+const Headline = styled(Typography)<BannerProps>((props) => {
+  const colors = getColors(props);
+
+  return css`
+    color: ${props.type === "background"
+      ? colors?.common.white
+      : colors?.common.black};
+
+    ${fontHeaderXxl(props)}
+  `;
+});
+
+const GradientBox = styled(Box)<BannerProps>((props) => {
+  return css`
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-image: ${props.type === "background"
+      ? props.theme.breakpoints.down("md")
+        ? "linear-gradient(0deg, rgba(0,0,0,0), rgba(0,0,0,1))"
+        : "linear-gradient(270deg, rgba(0,0,0,0), rgba(0,0,0,1))"
+      : "linear-gradient(0deg, rgba(255,255,255,1), rgba(255,255,255,1))"};
+    z-index: 1;
+  `;
+});
+
+const ImageBox = styled(Box)(({ theme }) => ({
+  position: "absolute",
+  top: 0,
+  width: "100%",
+}));
+
 export const Banner = ({
   headline,
-  page,
-  pagesComponent,
   image,
   type = "nobackground",
 }: BannerProps) => {
   return (
-    <Box
-      sx={{
-        position: "relative",
-        height: maxHeight,
-        width: "100%",
-        backgroundColor: "black",
-      }}
-    >
-      <Container sx={{ position: "relative", zIndex: 2, height: "100%" }}>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "left",
-            alignItems: { xs: "top", md: "center" },
-            height: "100%",
-            paddingTop: { xs: 7, md: 0 },
-            color: "white",
-          }}
-        >
-          <Typography
-            variant="h1"
-            sx={{
-              fontSize: { xs: "2rem", md: "2.5rem" },
-              lineHeight: "120%",
-              maxWidth: { xs: "100%", md: "60%" },
-            }}
-            color={type === "background" ? "white" : "black"}
-          >
+    <StyledBox type={type}>
+      <StyledContainer>
+        <ContentBox type={type}>
+          <Headline variant="h1" type={type}>
             {headline}
-          </Typography>
-        </Box>
-      </Container>
-      {/* Left black to transparent gradient */}
-      <Box
-        sx={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          height: "100%",
-          width: "100%",
-          backgroundImage:
-            type === "background"
-              ? {
-                  xs: "linear-gradient(0deg, rgba(0,0,0,0), rgba(0,0,0,1))",
-                  md: "linear-gradient(270deg, rgba(0,0,0,0), rgba(0,0,0,1))",
-                }
-              : type === "nobackground"
-              ? "linear-gradient(0deg, rgba(255,255,255,1), rgba(255,255,255,1))"
-              : "none",
-          zIndex: 1,
-        }}
-      />
-      <Box
-        sx={{
-          position: "absolute",
-          top: 0,
-          width: "100%",
-        }}
-      >
+          </Headline>
+        </ContentBox>
+      </StyledContainer>
+      <GradientBox type={type} />
+      <ImageBox>
         {type !== "nobackground" && (
           <Box sx={{ height: maxHeight }}>{image}</Box>
         )}
-      </Box>
-    </Box>
+      </ImageBox>
+    </StyledBox>
   );
 };
