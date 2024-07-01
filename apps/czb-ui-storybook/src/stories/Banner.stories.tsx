883f1fc8c15711a0232f329b1caa15d79edbb695
Kyle Awayan <kyle@awayan.com>
2024-07-01 10:05:53 -0700

    improve footer story

--- apps/czb-ui-storybook/src/stories/Banner.stories.tsx
+++ apps/czb-ui-storybook/src/stories/Banner.stories.tsx
@@ -2,28 +2,48 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import { Banner as CZBUIBanner } from "@czb-ui/core";
 
-const meta: Meta<typeof CZBUIBanner> = {
-  title: "core/Banner",
+const ImageComponent = ({ src }: { src: string }) => (
+  <img
+    style={{ objectFit: "cover", height: "100%", width: "100%", zIndex: 0 }}
+    src={src}
+  />
+);
+
+type BannerPropsAndCustomArgs = React.ComponentProps<typeof CZBUIBanner> & {
+  imageSrc?: string;
+};
+
+const meta: Meta<BannerPropsAndCustomArgs> = {
   component: CZBUIBanner,
+  title: "core/Banner",
   parameters: {
     layout: "fullscreen",
   },
+  argTypes: {
+    imageSrc: {
+      control: "text",
+      description: "Source URL of the image",
+    },
+    type: {
+      control: "radio",
+      options: ["background", "nobackground"],
+    },
+  },
+  render: ({ imageSrc, ...args }) => (
+    <CZBUIBanner {...args} image={<ImageComponent src={imageSrc || ""} />} />
+  ),
 };
 
 export default meta;
 
-type Story = StoryObj<typeof CZBUIBanner>;
+type Story = StoryObj<BannerPropsAndCustomArgs>;
 
 export const Banner: Story = {
   args: {
     headline:
       "Developing Technology to Push the Boundaries of What We Can See and Measure",
-    image: (
-      <img
-        style={{ objectFit: "cover", height: "100%", width: "100%", zIndex: 0 }}
-        src="https://www.czbiohub.org/sf/wp-content/uploads/sites/18/2023/02/home-sf-hero-desk.jpg"
-      />
-    ),
+    imageSrc:
+      "https://www.czbiohub.org/sf/wp-content/uploads/sites/18/2023/02/home-sf-hero-desk.jpg",
     type: "background",
   },
 };
