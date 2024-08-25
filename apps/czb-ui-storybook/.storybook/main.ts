8b3d12498d527adeb824e045e76062a5078da8e2
Kyle Awayan <kyle@awayan.com>
2024-08-25 14:02:21 -0700

    update storybook

--- apps/czb-ui-storybook/.storybook/main.ts
+++ apps/czb-ui-storybook/.storybook/main.ts
@@ -11,9 +11,7 @@ const config: StorybookConfig = {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+  docs: {},
 };
 export default config;
 
