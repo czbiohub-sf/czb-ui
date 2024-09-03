6fe8936c587e1acaf257647178a77ecb39566abc
Kyle Awayan <kyle@awayan.com>
2024-09-03 14:49:00 -0700

    WithTitleLight logo

--- packages/biohub-logos/src/index.ts
+++ packages/biohub-logos/src/index.ts
@@ -3,4 +3,5 @@ export { default as DnaMark } from "./SFColor"; // Depercated import name
 export { default as SFColor } from "./SFColor";
 export { default as SFColorReverse } from "./SFColorReverse";
 export { default as WithTitle } from "./WithTitle";
+export { default as WithTitleLight } from "./WithTitleLight";
 export { default as CZBMark } from "./CZBMark";
