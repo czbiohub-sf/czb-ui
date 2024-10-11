a000fd0062ee02fb3d1c02c582bd8389c89070c5
Kyle Awayan <kyle@awayan.com>
2024-10-10 18:34:42 -0700

    Revert "build esm only"
    This reverts commit bdd424bdd99427f0ae2f5988462d8554d0c5fa31.

--- packages/viz/rollup.config.ts
+++ packages/viz/rollup.config.ts
@@ -6,6 +6,10 @@ export default {
   input: "src/index.ts",
   output: [
     {
+      file: "dist/index.cjs.js",
+      format: "cjs",
+    },
+    {
       file: "dist/index.esm.js",
       format: "esm",
     },
