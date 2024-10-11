bdd424bdd99427f0ae2f5988462d8554d0c5fa31
Kyle Awayan <kyle@awayan.com>
2024-10-10 18:19:48 -0700

    build esm only

--- packages/viz/rollup.config.ts
+++ packages/viz/rollup.config.ts
@@ -6,10 +6,6 @@ export default {
   input: "src/index.ts",
   output: [
     {
-      file: "dist/index.cjs.js",
-      format: "cjs",
-    },
-    {
       file: "dist/index.esm.js",
       format: "esm",
     },
